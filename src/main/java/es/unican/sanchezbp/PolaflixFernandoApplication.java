29fe1d05e8cbd3bed8f31021e5685154ecc76bfc
Pablo Sanchez <sanchezbp@unican.es>
2021-03-11 17:59:29 +0100

    First commit

---
+++ src/main/java/es/unican/sanchezbp/PolaflixFernandoApplication.java
@@ -0,0 +1,13 @@
+package es.unican.sanchezbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolaflixFernandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolaflixFernandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/unican/sanchezbp/PolaflixFernandoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.sanchezbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixFernandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
